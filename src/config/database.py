ab195b842089323e3639dbe10105db0d1a02d2ee
fredimatteo <matteofredi.developer@gmail.com>
2024-04-23 15:44:20 +0200

    [feature] add healthcheck function in database.py

--- src/config/database.py
+++ src/config/database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy import exc as alchemy_exceptions
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from src.main import get_settings
+from src.config import settings
 
-st = get_settings()
+st = settings.get_settings()
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{st.db_user}:{st.db_password}@{st.db_host}:{st.db_port}/{st.db_name}"
 
@@ -20,8 +20,21 @@ def get_db():
     db = SessionLocal()
     try:
         yield db
-    except alchemy_exceptions.SQLAlchemyError as err:
+    except alchemy_exceptions.DatabaseError as err:
         print("An exception occurred: {}".format(err))
         db.rollback()
+    finally:
+        db.close()
+
+
+def healthcheck() -> str:
+    db = SessionLocal()
+    try:
+        # Try to create a session and execute a simple query
+        db.execute(text("SELECT 1"))
+        return "Ok"
+    except alchemy_exceptions.OperationalError:
+        # If there's an error connecting to the database, return False
+        return "Offline"
     finally:
         db.close()
