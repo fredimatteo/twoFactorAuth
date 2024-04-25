f9b368d6f7503fe65c7741057ce9e10b5914c71b
fredimatteo <matteofredi.developer@gmail.com>
2024-04-25 21:26:55 +0200

    [feature] add HTTPBearer security

--- src/api/v1/user_api.py
+++ src/api/v1/user_api.py
@@ -1,18 +1,33 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from src.config import database
+from src.config.database import SessionLocal
+from src.models.user_model import User
 from src.schemas import user_schema
-from src.services import user as user_service
+from src.services import user as user_service, auth as auth_service
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
+security = HTTPBearer()
+
+
+async def authenticate(credential: HTTPAuthorizationCredentials = Security(security)):
+    try:
+        db = SessionLocal()
+        user = auth_service.decode_token(credential.credentials, db)
+        return user
+    except Exception as e:
+        raise e
+
+
 @router.get("", response_model=List[user_schema.UserResponseSchema])
-def get_users(db: Session = Depends(database.get_db)):
-    users = user_service.get_all_users(db)
+def get_users(db: Session = Depends(database.get_db), current_user: User = Depends(authenticate)):
+    users = user_service.get_all_users(db, current_user)
 
     return users
 
@@ -24,4 +39,3 @@ def create_user(src: user_schema.UserCreateSchema, db: Session = Depends(databas
     db.commit()
 
     return user
-    
